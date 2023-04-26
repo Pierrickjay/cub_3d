7cb8ce9ab21825faa3585add48de7f6c5a3b1426
rertzer <rertzer@student.42.fr>
2023-04-26 13:54:05 +0200

    norminet

--- srcs/cub_3d/game/cat_bonus.c
+++ srcs/cub_3d/game/cat_bonus.c
@@ -6,33 +6,31 @@
 /*   By: pjay <pjay@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 09:25:32 by pjay              #+#    #+#             */
-/*   Updated: 2023/04/26 13:27:16 by rertzer          ###   ########.fr       */
+/*   Updated: 2023/04/26 13:53:18 by rertzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub_3d.h"
 #include "cub_3d_bonus.h"
 
-static t_point	set_cat(t_cbdata *data, int n);
-static t_point	set_screen(t_cbdata *data, t_point cat);
+static t_point		set_cat(t_cbdata *data, int n);
+static t_point		set_screen(t_cbdata *data, t_point cat);
 static t_app_cat	set_app_cat(float angle, float dist);
 
 void	put_cat(t_cbdata *data)
 {
 	t_app_cat	kitty;
-	int		current;
-	float	left;
-	t_point	cat;
-	t_point	screen;
-
+	int			current;
+	float		left;
+	t_point		cat;
+	t_point		screen;
 
 	cat = set_cat(data, 0);
 	screen = set_screen(data, cat);
-
 	if (screen.angle > M_PI_2 && screen.angle < 3 * M_PI_2)
 		return ;
 	kitty = set_app_cat(screen.angle, cat.dist);
-	left = screen.x - kitty.x / 2.0; 
+	left = screen.x - kitty.x / 2.0;
 	while (kitty.offset_x <= (int)kitty.x)
 	{
 		current = (int)left + kitty.offset_x;
@@ -45,14 +43,14 @@ void	put_cat(t_cbdata *data)
 static t_point	set_cat(t_cbdata *data, int n)
 {
 	t_point	cat;
-	
+
 	cat.x = data->cats[n].pos.x - data->pos_x;
 	cat.y = data->cats[n].pos.y - data->pos_y;
 	cat.angle = atanf(-cat.y / cat.x);
 	if (cat.x < 0)
 		cat.angle += M_PI;
 	cat.angle = fmod(cat.angle + TWO_PI, TWO_PI);
-	cat.dist = sqrt(cat.x * cat.x + cat.y *cat.y);
+	cat.dist = sqrt(cat.x * cat.x + cat.y * cat.y);
 	return (cat);
 }
 
--- srcs/cub_3d/game/draw_slice.c
+++ srcs/cub_3d/game/draw_slice.c
@@ -6,14 +6,14 @@
 /*   By: rertzer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 12:54:56 by rertzer           #+#    #+#             */
-/*   Updated: 2023/04/26 13:45:44 by rertzer          ###   ########.fr       */
+/*   Updated: 2023/04/26 13:51:11 by rertzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub_3d.h"
 #include "cub_3d_bonus.h"
 
-static void	put_slice(t_cbdata *data, t_vec pixel, int current_x, int current_y);
+static void	put_slice(t_cbdata *data, t_vec pixel, int cur_x, int cur_y);
 
 void	draw_slice(t_cbdata *data, int current_x, t_app_cat kitty)
 {
@@ -22,11 +22,11 @@ void	draw_slice(t_cbdata *data, int current_x, t_app_cat kitty)
 	t_vec	pixel;
 
 	pixel.x = kitty.offset_x / kitty.reduct;
-	ground = PLANE_Y /2  + BLOCK_SIZE / 2 * kitty.reduct;
+	ground = PLANE_Y / 2 + BLOCK_SIZE / 2 * kitty.reduct;
 	while (kitty.offset_y < kitty.y)
 	{
 		current_y = ground - kitty.offset_y;
-		if ( current_y >= 0 && current_y < PLANE_Y)
+		if (current_y >= 0 && current_y < PLANE_Y)
 		{
 			pixel.y = SPRITE_H - kitty.offset_y / kitty.reduct;
 			put_slice(data, pixel, current_x, current_y);
@@ -35,14 +35,14 @@ void	draw_slice(t_cbdata *data, int current_x, t_app_cat kitty)
 	}
 }
 
-static void	put_slice(t_cbdata *data, t_vec pixel, int current_x, int current_y)
+static void	put_slice(t_cbdata *data, t_vec pixel, int cur_x, int cur_y)
 {
 	char	*cat_pixel;
 
 	cat_pixel = data->texture.cat[0].addr + (int)pixel.y \
-				* data->texture.cat[0].line_len  \
+				* data->texture.cat[0].line_len \
 				+ (int)pixel.x * data->texture.cat[0].bpp / 8;
 	if ((*(unsigned int *)cat_pixel & 0x00FFFFF) != 0)
-		my_mlx_pixel_put(data, current_x, current_y, \
+		my_mlx_pixel_put(data, cur_x, cur_y, \
 				*(unsigned int *)cat_pixel);
 }
