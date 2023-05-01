42c74d22f43e8b842702b232de76b46671b52769
Pierrick Jay <pierrick.jay1@gmail.com>
2023-05-01 16:17:36 +0200

    Finish

--- srcs/cub_3d/game/change_pos.c
+++ srcs/cub_3d/game/change_pos.c
@@ -6,7 +6,7 @@
 /*   By: pjay <pjay@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 10:56:00 by pjay              #+#    #+#             */
-/*   Updated: 2023/05/01 12:46:58 by rertzer          ###   ########.fr       */
+/*   Updated: 2023/05/01 13:02:06 by pjay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	change_view_player(t_cbdata *data)
 {
 	if (data->keypressed[right_view])
 	{
-		data->angle = fmod(data->angle - data->keypressed[right_view] * 20.0 * \
+		data->angle = fmod(data->angle - data->keypressed[right_view] * 40.0 * \
 				ANGLE_PACE + TWO_PI, TWO_PI);
 	}
 }
