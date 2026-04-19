243b9424bd0d6723f22d43a613faed7c8628eb0b
rertzer <rertzer@student.42.fr>
2026-04-19 17:46:30 +0200

    indentatation

--- srcs/cub_3d/game/change_pos.c
+++ srcs/cub_3d/game/change_pos.c
@@ -12,45 +12,41 @@
 
 #include "cub_3d.h"
 
-bool	bump_wall(t_cbdata *data, int new_x, int new_y)
-{
-	new_x /= BLOCK_SIZE;
-	new_y /= BLOCK_SIZE;
-	if (!cell_inside(data, new_x, new_y))
-		return (false);
-	if (cell_isa(data->map[new_y][new_x], CELL_HARD))
-		return (true);
-	else
-		return (false);
+bool bump_wall(t_cbdata *data, int new_x, int new_y) {
+  new_x /= BLOCK_SIZE;
+  new_y /= BLOCK_SIZE;
+  if (!cell_inside(data, new_x, new_y))
+    return (false);
+  if (cell_isa(data->map[new_y][new_x], CELL_HARD))
+    return (true);
+  else
+    return (false);
 }
 
-void	reset_new_pos(t_point *new_pos, float value, float angle)
-{
-	new_pos->x -= 4 * value * cos(angle);
-	new_pos->y += 4 * value * sin(angle);
+void reset_new_pos(t_point *new_pos, float value, float angle) {
+  new_pos->x -= 4 * value * cos(angle);
+  new_pos->y += 4 * value * sin(angle);
 }
 
-void	change_pos_player(t_cbdata *data)
-{
-	t_point	new_pos;
+void change_pos_player(t_cbdata *data) {
+  t_point new_pos;
 
-	new_pos.x = data->pos_x;
-	new_pos.y = data->pos_y;
-	if (data->keypressed[down])
-		reset_new_pos(&new_pos, data->keypressed[down], data->angle);
-	if (data->keypressed[right])
-		reset_new_pos(&new_pos, data->keypressed[right], data->angle + M_PI_2);
-	if (!bump_wall(data, (int)new_pos.x, (int)data->pos_y))
-		data->pos_x = new_pos.x;
-	if (!bump_wall(data, (int)data->pos_x, (int)new_pos.y))
-		data->pos_y = new_pos.y;
+  new_pos.x = data->pos_x;
+  new_pos.y = data->pos_y;
+  if (data->keypressed[down])
+    reset_new_pos(&new_pos, data->keypressed[down], data->angle);
+  if (data->keypressed[right])
+    reset_new_pos(&new_pos, data->keypressed[right], data->angle + M_PI_2);
+  // if (!bump_wall(data, (int)new_pos.x, (int)data->pos_y))
+  data->pos_x = new_pos.x;
+  // if (!bump_wall(data, (int)data->pos_x, (int)new_pos.y))
+  data->pos_y = new_pos.y;
 }
 
-void	change_view_player(t_cbdata *data)
-{
-	if (data->keypressed[right_view])
-	{
-		data->angle = fmod(data->angle - data->keypressed[right_view] * 40.0 * \
-				ANGLE_PACE + TWO_PI, TWO_PI);
-	}
+void change_view_player(t_cbdata *data) {
+  if (data->keypressed[right_view]) {
+    data->angle = fmod(
+        data->angle - data->keypressed[right_view] * 40.0 * ANGLE_PACE + TWO_PI,
+        TWO_PI);
+  }
 }
